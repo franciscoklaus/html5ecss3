f2d0c48a68207cd3c0997dcfa03fb792c2b029e0
josé Francisco <franciscoklausfilho@hotmail.com>
2021-12-01 15:03:15 -0300

    tipos de input

--- index2.php
+++ index2.php
@@ -20,5 +20,25 @@
 
     <!-- serve apenas para auxiliar o motor de busca -->
     O meu dia preferido é o <time datetime='1987-03-16'>dia do meu aniversário.</time>
+    <br/>
+    <br/>
+    <form action="POST">
+        <input type="text" name="texto" id="" placeholder='text'><br/><br/>
+        <input type="search" name="search" id="" placeholder="search"><br/><br/>
+        <input type="email" name="email" id=""  placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required><br/><br/>
+        <input type="url" name='url' id='' placeholder="url" ><br/><br/>
+        <input type="tel" name="telefone" id=""><br/><br/>
+        <input type="number" name="number" id=""><br/><br/>
+        <input type="range" name="range" min='0' max='100' id=""><br/><br/>
+        <input type="date" name="data" id="" value='2021-03-01'><br/><br/>
+        <input type="datetime-local" name="date-time" id=""><br/><br/>
+        <input type="month" name="month" id=""><br/><br/>
+        <input type="week" name="week" id=""><br/><br/>
+        <input type="time" name="time" id=""><br/><br/>
+        <input type="color" name="color" id=""><br/><br/>
+        <input type="submit" value="Enviar">
+    </form>
+    
+
 </body>
 </html>
